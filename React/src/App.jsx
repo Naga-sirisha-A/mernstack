40b2948df8fb3a8e4e4ba770cb39a1ebf5ba2fbd
Naga-sirisha-A <aangadinagasirisha@gmail.com>
2025-05-30 14:53:52 +0530

    created navbar

---
+++ React/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./navbar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      {/* You can add more components below */}
+      <h1>Welcome to My Website</h1>
+    </div>
+  );
+}
+
+export default App;
